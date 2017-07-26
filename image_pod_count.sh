b04d573f664de476e8393cac69de0d59cd0df508
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2017-07-26 13:35:57 +0100

    Added direct DC deployment count.

--- image_pod_count.sh
+++ image_pod_count.sh
@@ -10,7 +10,8 @@ dcCount=0;
 
 for project in $(oc get projects -o jsonpath='{.items[*].metadata.name}');
 do
-  #echo $project;
+  echo "PROCESSING BC for "$project;
+
   for bc in $(oc get bc -n $project -o jsonpath='{.items[*].metadata.name}');
   do
     imageName=$(oc get bc $bc -n $project -o jsonpath='{$.spec.strategy..from.name}');
@@ -25,6 +26,23 @@ do
       bcCount=$(($bcCount+$dc_count));
     fi
   done
+
+  echo "PROCESSING DC for "$project;
+
+  for dc in $(oc get dc -n $project -o jsonpath='{.items[*].metadata.name}');
+  do
+    dcImageName=$(oc get dc $dc -n $project -o jsonpath='{.spec.template.spec..image}');
+
+    #echo "DEBUG "$dcImageName;
+    if grep -q $1 <<<$dcImageName; then
+
+      dc_dc_count=$(oc get dc $dc -n $project -o jsonpath='{.status.replicas}');
+
+      echo "[Found] Direct DC deployment replicas "$dc_dc_count" of "$dcImageName;
+      dcCount=$((dcCount+$dc_dc_count));
+    fi
+  done
 done
 
 echo "Total of Pods via BC match: "$bcCount
+echo "Total of Pods via direct DC match: "$dcCount
