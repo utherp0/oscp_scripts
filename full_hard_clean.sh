21c6985ea57aea9b757a1a27080af5cd52913686
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2017-08-31 12:01:55 +0100

    Corrected full_hard_clean.sh

--- full_hard_clean.sh
+++ full_hard_clean.sh
@@ -1,12 +1,4 @@
-# Parameter check
-if [ -z "$1" ]; then
-  echo "Usage: ./(script) TARGET_NAMESPACE";
-  echo 'WARNING. This script hard cleans ALL projects.";
-  echo "Also - this script requires the hard_clean.sh script to be in the same directory."
-  exit 0
-fi
-
-for project in $(oc get projects -n $1 -o jsonpath='{.items[*].metadata.name}');
+for project in $(oc get projects -o jsonpath='{.items[*].metadata.name}');
 do
   echo "Processing project "$project;
 
