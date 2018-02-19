4b627503413cd8a155b324a5af2b5d8f9c0cabd4
HANDS Richard <richard.hands@soprasteria.com>
2018-02-19 12:52:04 +0000

    change to leave text as normal, not bold, and to also mark conditions as red if they are Unknown in case of node problems

--- nodes.sh
+++ nodes.sh
@@ -5,6 +5,6 @@ do
   NODEDESC=$(oc describe node $node);
   grep Pods: <<< "$NODEDESC";
   STATUS=$(grep -A6 Conditions <<< "$NODEDESC" | grep -A4 "\-\-\-\-" | awk 'NR> 1{ print $1;print $2 }' | tr "\n" " ");
-  echo "$STATUS" | awk '{ printf($2 == "True") ? "\033[1;31m OutOfDisk \033[1;39m" : "\033[1;32m OutOfDisk \033[1;39m"; printf($4 == "True") ? "\033[1;31m MemoryPressure \033[1;39m" : "\033[1;32m MemoryPressure \033[1;39m"; printf($6 == "True") ? "\033[1;31m DiskPressure \033[1;39m" : "\033[1;32m DiskPressure \033[1;39m"; printf($8 == "False") ? "\033[1;31m Ready \033[1;39m" : "\033[1;32m Ready \033[1;39m"}'
+  echo "$STATUS" | awk '{ printf($2 == "True" || $2 == "Unknown") ? "\033[1;31m OutOfDisk \033[0;39m" : "\033[1;32m OutOfDisk \033[0;39m"; printf($4 == "True" || $4 == "Unknown") ? "\033[1;31m MemoryPressure \033[0;39m" : "\033[1;32m MemoryPressure \033[0;39m"; printf($6 == "True" || $6 == "Unknown") ? "\033[1;31m DiskPressure \033[0;39m" : "\033[1;32m DiskPressure \033[0;39m"; printf($8 == "False" || $8 == "Unknown") ? "\033[1;31m Ready \033[0;39m" : "\033[1;32m Ready \033[0;39m"}'
   echo;
 done
