e086ed10e5ad120b06b34126d2928b3428933275
Richard Stuart Hands <richardhands@googlemail.com>
2018-12-06 10:02:55 +0000

    Update to support variable condition location
    in newer versions of openshift the condition locations can be in different positions which triggers false 'red' flags, so made the awk script look for each field position by name in order that this is avoided.  also added the new PIDPressure flag and made the node name print in blue so it's visually easier to read the output.

--- nodes.sh
+++ nodes.sh
@@ -1,10 +1,19 @@
 oc get nodes
 for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}');
 do
-  echo $node;
+  echo -e "\033[1;34m$node\033[0;39m";
   NODEDESC=$(oc describe node $node);
   grep Pods: <<< "$NODEDESC";
-  STATUS=$(grep -A6 Conditions <<< "$NODEDESC" | grep -A4 "\-\-\-\-" | awk 'NR> 1{ print $1;print $2 }' | tr "\n" " ");
-  echo "$STATUS" | awk '{ printf($2 == "True" || $2 == "Unknown") ? "\033[1;31m OutOfDisk \033[0;39m" : "\033[1;32m OutOfDisk \033[0;39m"; printf($4 == "True" || $4 == "Unknown") ? "\033[1;31m MemoryPressure \033[0;39m" : "\033[1;32m MemoryPressure \033[0;39m"; printf($6 == "True" || $6 == "Unknown") ? "\033[1;31m DiskPressure \033[0;39m" : "\033[1;32m DiskPressure \033[0;39m"; printf($8 == "False" || $8 == "Unknown") ? "\033[1;31m Ready \033[0;39m" : "\033[1;32m Ready \033[0;39m"}'
+  grep Roles: <<< "$NODEDESC";
+  STATUS=$(grep -A7 Conditions <<< "$NODEDESC" | grep -A5 "\-\-\-\-" | awk 'NR> 1{ print $1;print $2 }' | tr "\n" " ");
+
+  echo "$STATUS" | awk '{
+for(i=1;i<=NF;i++){if ($i ~ /OutOfDisk/) { printf($i+1 == "True" || $i+1 == "Unknown") ? "\033[1;31m OutOfDisk \033[0;39m" : "\033[1;32m OutOfDisk \033[0;39m"}};
+for(i=1;i<=NF;i++){if ($i ~ /MemoryPressure/) {printf($i+1 == "True" || $i+1 == "Unknown") ? "\033[1;31m MemoryPressure \033[0;39m" : "\033[1;32m MemoryPressure \033[0;39m"}};
+for(i=1;i<=NF;i++){if ($i ~ /DiskPressure/) {printf($i+1 == "True" || $i+1 == "Unknown") ? "\033[1;31m DiskPressure \033[0;39m" : "\033[1;32m DiskPressure \033[0;39m"}};
+for(i=1;i<=NF;i++){if ($i ~ /Ready/) {printf($i+1 == "False" || $i+1 == "Unknown") ? "\033[1;31m Ready \033[0;39m" : "\033[1;32m Ready \033[0;39m"}};
+for(i=1;i<=NF;i++){if ($i ~ /PIDPressure/) {printf($i+1 == "True" || $i+1 == "Unknown") ? "\033[1;31m PIDPressure \033[0;39m" : "\033[1;32m PIDPressure \033[0;39m";}}
+}'
+
   echo;
 done
